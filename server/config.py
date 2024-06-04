e68a3c9cafd2d7d4ebf87a25ddc29f058f118f3d
alvarosh15 <alvaro.s.h.15@gmail.com>
2024-06-04 09:56:49 +0200

    Added requirements.txt and gunicorn

--- server/config.py
+++ server/config.py
@@ -7,10 +7,11 @@ class Config:
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
     JWT_ACCESS_TOKEN_EXPIRES = 86400  # 1 día
     
-    DB_HOST = os.getenv('DB_HOST', 'localhost')
-    DB_USER = os.getenv('DB_USER', 'user')
-    DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
-    DB_NAME = os.getenv('DB_NAME', 'dbname')
-    
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+    DB_HOST = os.getenv('DB_HOST')
+    DB_USER = os.getenv('DB_USER')
+    DB_PORT = os.getenv('DB_PORT')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_NAME = os.getenv('DB_NAME')
+
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
